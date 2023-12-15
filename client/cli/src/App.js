ba33297cf7db71ecae9790393e0c327f80cc39f5
Harsh Agarwal <harshulkh@gmail.com>
2023-12-16 04:24:48 +0530

    formatting

--- client/cli/src/App.js
+++ client/cli/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Homepage from "./comp/Homepage";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<Homepage />
+		</div>
+	);
 }
 
 export default App;
