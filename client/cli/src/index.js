e7c247aa7448894f2e1b9da2939126ebf095449d
Harsh Agarwal <harshulkh@gmail.com>
2023-12-16 11:52:03 +0530

    updation

--- client/cli/src/index.js
+++ client/cli/src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Orgchat from "./hooks/Orgchat";
+import ParamsStorage from "./hooks/ParamsStorage";
+import "./bootstrap.min.js";
+import "./jquery-3.2.1.min.js";
+import "./jquery.cookie.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+	<div>
+		<App />
+		<Orgchat />
+		<ParamsStorage />
+	</div>
+);
